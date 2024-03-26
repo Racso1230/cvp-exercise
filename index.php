a274f9e94883639920e545b33fdc01c97172ed6a
Racso1230 <oscarfreeman@live.co.uk>
2024-03-26 16:33:58 +0000

    initial navbar

--- index.php
+++ index.php
@@ -3,11 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="static/static.css" type="text">
+    <link  type="text/css"rel="stylesheet" href="static/static.css">
     <title>CVP Exercise</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+
+<div class= "nav">
+    <a class="active" href="https://cvp.com/department/cameras">Cameras</a>
+    <a href="https://cvp.com/department/lenses">Lenses</a>
+    <a href="https://cvp.com/department/accessories">Accessories</a>
+    <a href="https://cvp.com/department/audio">Audio</a>
+    <a href="https://cvp.com/department/monitoring">Monitoring</a>
+    <a href="https://cvp.com/department/lighting">Lighting</a>
+    <a href="https://cvp.com/department/support-motion">Support and Motion</a>
+    <a href="https://cvp.com/department/studio">Studio</a>
+</div>
+    <h1>CVP Product Catalogue</h1>
 </body>
 </html>
 

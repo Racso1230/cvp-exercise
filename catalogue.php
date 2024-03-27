deed502cd565501cdddaf6baadb8d8d3916d5e56
Racso1230 <oscarfreeman@live.co.uk>
2024-03-27 20:46:54 +0000

    added some comments-will try to do more as I go along

--- catalogue.php
+++ catalogue.php
@@ -2,11 +2,17 @@
 
 class Products {
     private $db;
-    
+    //used a constructore as I want to make absolute sure a pdo object gets passed through
     public function __construct(PDO $db) {
         $this->db = $db;
     }
+    //used to retrieve all products from the database cvp_exercise
 
+    /*
+    $stmt acts as a way to get all of the products from the cvp_exercise table by using a simple sql statement
+    the execute() function just executes the vairable (doesn't need any params so that is useful)
+    return the $stmt variable so I can use fetchAll() function which displays the data in an array
+    */ 
     public function getAllProducts () {
         $stmt = $this->db->prepare("SELECT * FROM cvp_exercise");
         $stmt->execute();
--- index.php
+++ index.php
@@ -28,16 +28,25 @@
 </html>
 
 <?php
-
+//includes the files that connect to the database and that allows me to grab data from the cvp_exercise table
 include 'db_connect.php';
 include 'catalogue.php';
 
+//calls the database connection class and calls the connect() function within the object
 $dbObject = new Database();
 $connect = $dbObject->connect();
 
+/*
+calls the products class from within catalogue.php but also passes the dbo object (linking the pdo object to the retrieval object) within the function
+$products vairable is used to call the function that displays that fetches the data from a simple sql select query into an array (from the catalogue.php return statement)
+*/
 $productInstance = new Products($connect);
 $products = $productInstance->getAllProducts();
 
+/*
+used a foreach statement to loop through each selected row from the data as the data is in an array data type
+the htmlspecialchars function is used for good practice-helps prevent XSS attacks and is generally encouraged from what I understand
+*/
 foreach($products as $product) {
     echo "Product ID: " . htmlspecialchars($product['productID']) . "<br>";
     echo "Product Name: " . htmlspecialchars($product['product_name']) . "<br>";
